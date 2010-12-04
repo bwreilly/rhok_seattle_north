5ad903b0e539ddfdaa65c55afac07aec46b7f360
Kevin Moore <kevin@thinkpixellab.com>
2010-12-04 10:58:09 -0800

    Rails in dah house...

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RhokSeattleNorth::Application.config.secret_token = '26da36ddedc4262992755683cdc17ac419b419bc530f10eaba3f365c566c017524884f4fd73b7987e231a4578780da95120494271f282a164beea54aad1a4d26'
