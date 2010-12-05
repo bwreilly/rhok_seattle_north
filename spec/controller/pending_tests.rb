c96061474ef134d55dc76b6f81ca157b3f932f01
bwreilly <gallipoli@gmail.com>
2010-12-05 11:23:51 -0800

    some quick ux tests

---
+++ spec/controller/pending_tests.rb
@@ -0,0 +1,10 @@
+pending "when the user submits a placename a disambiguation page appears, if
+  more than one result"
+pending "disambiguation page lists closest matches to placename, "
+pending "user enters search radius and correct placename at disambiguation page
+  result page shown"
+pending "results include placename and largest population centers within radius"
+pending "results are displayed as a table"
+pending "result page includes an export to csv function"
+pending "result table includes wikipedia link to each result"
+pending "result table includes osm link to each result"
