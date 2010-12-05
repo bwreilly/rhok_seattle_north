54f92aa45bc4ea7c1e18a192e6ee25e8f64761fe
Scott Harris <sharris@Scott-Harriss-MacBook-Pro.local>
2010-12-04 16:43:49 -0800

    added refine and results view
    mocked up classes in controller

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,12 @@
 class MainController < ApplicationController
   def index
   end
+
+  def refine
+    @query = params['q']
+    @results = ['fi','fie','foe','foo']
+  end
+
+  def results
+  end
 end
